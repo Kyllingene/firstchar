7badb86291ed2ed3ce87a6dae6024c60222fd3a2
Kyllingene <fuzzymuffin343@gmail.com>
2023-03-21 15:48:50 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::io;
+
+fn main() {
+    let args = env::args();
+
+    let text = if args.len() >= 2 {
+        args.skip(1).collect::<Vec<String>>().join(" ")
+    } else {
+        io::stdin().lines().filter_map(|l| l.ok()).collect::<Vec<String>>().join("\n")
+    };
+
+    let mut short = String::new();
+    let mut chs = text.chars().peekable();
+
+    if let Some(ch) = chs.next() {
+        short.push(ch);
+    }
+
+    while let Some(ch) = chs.next() {
+        if ch.is_ascii_punctuation() || ch == '\n' || ch == '\t' {
+            short.push(ch);
+
+            if let Some(ch) = chs.peek() {
+                if !ch.is_ascii_punctuation() || *ch == '\n' || *ch == '\t' {
+                    short.push(*ch);
+                }
+            }
+        } else if ch == ' ' {
+            if let Some(ch) = chs.peek() {
+                if !ch.is_ascii_punctuation() || *ch == '\n' || *ch == '\t' {
+                    short.push(*ch);
+                }
+            }
+        }
+    }
+
+    println!("{short}");
+}
